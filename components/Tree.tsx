07dc3bb5e8592603e438a4020417114c295df436
Jarren San Jose <sanjose.jarren@gmail.com>
2026-04-09 00:34:56 -0700

    style: revert trees to original cone design

--- components/Tree.tsx
+++ components/Tree.tsx
@@ -6,38 +6,31 @@ interface TreeProps {
 }
 
 export default function Tree({ position = [0, 0, 0], scale = 1 }: TreeProps) {
-  const trunkColor = "#6B4423";
-  const leavesLight = "#8FBC8F";
-  const leavesMid = "#5DB075";
-  const leavesDark = "#3A9D5C";
+  const trunkColor = "#8B4513";
+  const leavesColor = "#228B22";
 
   return (
     <group position={position} scale={scale}>
-      {/* Trunk - simple cylinder */}
-      <mesh position={[0, 0.6, 0]} castShadow>
-        <cylinderGeometry args={[0.12, 0.18, 1.2, 12]} />
+      {/* Trunk - cylinder */}
+      <mesh position={[0, 0.5, 0]} castShadow>
+        <cylinderGeometry args={[0.15, 0.2, 1, 8]} />
         <meshStandardMaterial color={trunkColor} />
       </mesh>
 
-      {/* Foliage - soft rounded blobs */}
-      <mesh position={[0, 1.6, 0]} castShadow>
-        <sphereGeometry args={[0.9, 16, 12]} />
-        <meshStandardMaterial color={leavesMid} />
+      {/* Leaves - stacked cones for low-poly look */}
+      <mesh position={[0, 1.3, 0]} castShadow>
+        <coneGeometry args={[0.8, 1.2, 8]} />
+        <meshStandardMaterial color={leavesColor} />
       </mesh>
 
-      <mesh position={[0.4, 1.9, 0.3]} castShadow>
-        <sphereGeometry args={[0.55, 16, 12]} />
-        <meshStandardMaterial color={leavesLight} />
+      <mesh position={[0, 2.0, 0]} castShadow>
+        <coneGeometry args={[0.6, 1.0, 8]} />
+        <meshStandardMaterial color={leavesColor} />
       </mesh>
 
-      <mesh position={[-0.35, 2.0, -0.2]} castShadow>
-        <sphereGeometry args={[0.5, 16, 12]} />
-        <meshStandardMaterial color={leavesDark} />
-      </mesh>
-
-      <mesh position={[0.1, 2.3, 0]} castShadow>
-        <sphereGeometry args={[0.45, 16, 12]} />
-        <meshStandardMaterial color={leavesLight} />
+      <mesh position={[0, 2.6, 0]} castShadow>
+        <coneGeometry args={[0.4, 0.8, 8]} />
+        <meshStandardMaterial color={leavesColor} />
       </mesh>
     </group>
   );
